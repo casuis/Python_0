db3d5e204768487102a6e1202c023c7c8246cd67
Arthur SIMON <asimon@f1br2s6.clusters.42paris.fr>
2025-10-02 19:38:59 +0200

    feature: add ex02

--- ex02/find_ft_type.py
+++ ex02/find_ft_type.py
@@ -1,2 +1,17 @@
 def all_thing_is_obj(object: any) -> int:
-    return
+    match object:
+        case list():
+            print("List : ", end="")
+        case tuple():
+            print('Tuple : ', end="")
+        case set():
+            print('Set : ', end="")
+        case dict():
+            print('Dict : ', end="")
+        case str():
+            print(f'{object} is in the kitchen : ', end="")
+        case _:
+            print('Type not found')
+            return 42
+    print(f'{type(object)}')
+    return 42
